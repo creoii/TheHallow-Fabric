96f2305c7f72e2739ebde7d81443e27b2ca90d6c
creoii <33011337+creoii@users.noreply.github.com>
2021-07-03 23:20:54 -0400

    fixing data pack errors

--- src/main/java/creoii/hallows/core/registry/SurfaceRegistry.java
+++ src/main/java/creoii/hallows/core/registry/SurfaceRegistry.java
@@ -5,7 +5,10 @@ import creoii.hallows.common.world.surface.HallowedSurfaceBuilder;
 import creoii.hallows.core.Hallows;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.surfacebuilder.ConfiguredSurfaceBuilder;
 import net.minecraft.world.gen.surfacebuilder.SurfaceBuilder;
 import net.minecraft.world.gen.surfacebuilder.TernarySurfaceConfig;
@@ -16,19 +19,25 @@ public class SurfaceRegistry {
     public static final TernarySurfaceConfig GRASS_DIRT_HALLOWED_DIRT_CONFIG = new TernarySurfaceConfig(Blocks.GRASS_BLOCK.getDefaultState(), Blocks.DIRT.getDefaultState(), BlockRegistry.HALLOWED_DIRT.getDefaultState());
     public static final TernarySurfaceConfig PETRIFIED_SAND_CONFIG = new TernarySurfaceConfig(BlockRegistry.PETRIFIED_SAND.getDefaultState(), BlockRegistry.PETRIFIED_SANDSTONE.getDefaultState(), BlockRegistry.PETRIFIED_SANDSTONE.getDefaultState());
 
-    public static SurfaceBuilder<TernarySurfaceConfig> HALLOWED_SURFACE;
-    public static SurfaceBuilder<TernarySurfaceConfig> GLACIER_SURFACE;
+    public static final SurfaceBuilder<TernarySurfaceConfig> HALLOWED_SURFACE = new HallowedSurfaceBuilder(TernarySurfaceConfig.CODEC);
+    public static final SurfaceBuilder<TernarySurfaceConfig> GLACIER_SURFACE = new GlacierSurfaceBuilder(TernarySurfaceConfig.CODEC);
+
+    public static final RegistryKey<SurfaceBuilder<?>> HALLOWED_SURFACE_KEY = RegistryKey.of(Registry.SURFACE_BUILD_KEY, new Identifier(Hallows.MOD_ID, "hallowed"));
+    public static final RegistryKey<SurfaceBuilder<?>> GLACIER_SURFACE_KEY = RegistryKey.of(Registry.SURFACE_BUILD_KEY, new Identifier(Hallows.MOD_ID, "glacier"));
 
     public static ConfiguredSurfaceBuilder<?> HALLOWED;
     public static ConfiguredSurfaceBuilder<?> GLACIER;
     public static ConfiguredSurfaceBuilder<?> PETRIFIED;
 
     public static void register() {
-        HALLOWED_SURFACE = Registry.register(Registry.SURFACE_BUILDER, new Identifier(Hallows.MOD_ID, "hallowed"), new HallowedSurfaceBuilder(TernarySurfaceConfig.CODEC));
-        GLACIER_SURFACE = Registry.register(Registry.SURFACE_BUILDER, new Identifier(Hallows.MOD_ID, "glacier"), new GlacierSurfaceBuilder(TernarySurfaceConfig.CODEC));
+        Registry.register(Registry.SURFACE_BUILDER, HALLOWED_SURFACE_KEY.getValue(), HALLOWED_SURFACE);
+        Registry.register(Registry.SURFACE_BUILDER, GLACIER_SURFACE_KEY.getValue(), GLACIER_SURFACE);
 
         HALLOWED = HALLOWED_SURFACE.withConfig(HALLOWED_DIRT_CONFIG);
         GLACIER = GLACIER_SURFACE.withConfig(SurfaceBuilder.SAND_CONFIG);
         PETRIFIED = SurfaceBuilder.DEFAULT.withConfig(PETRIFIED_SAND_CONFIG);
+        Registry.register(BuiltinRegistries.CONFIGURED_SURFACE_BUILDER, new Identifier(Hallows.MOD_ID, "hallowed"), HALLOWED);
+        Registry.register(BuiltinRegistries.CONFIGURED_SURFACE_BUILDER, new Identifier(Hallows.MOD_ID, "glacier"), GLACIER);
+        Registry.register(BuiltinRegistries.CONFIGURED_SURFACE_BUILDER, new Identifier(Hallows.MOD_ID, "petrified"), PETRIFIED);
     }
 }
