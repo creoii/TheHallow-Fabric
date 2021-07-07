f7a8c6c355e5b7bef7009f037dcd696f83c3f172
creoii <33011337+creoii@users.noreply.github.com>
2021-07-06 22:06:38 -0400

    tags

--- src/main/java/creoii/hallows/core/util/Tags.java
+++ src/main/java/creoii/hallows/core/util/Tags.java
@@ -1,9 +1,27 @@
 package creoii.hallows.core.util;
 
+import creoii.hallows.core.Hallows;
+import net.fabricmc.fabric.api.tag.TagRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.tag.BlockTags;
+import net.minecraft.tag.ItemTags;
+import net.minecraft.tag.Tag;
+import net.minecraft.util.Identifier;
+
 public class Tags {
     public static class Blocks {
+        public static final Tag.Identified<Block> BASE_STONE_HALLOW = TagRegistry.create(new Identifier(Hallows.MOD_ID, "base_stone_hallow"), BlockTags::getTagGroup);
+        public static final Tag.Identified<Block> INFINIBURN_HALLOW = TagRegistry.create(new Identifier(Hallows.MOD_ID, "infiniburn_hallow"), BlockTags::getTagGroup);
+        public static final Tag.Identified<Block> ASPHODEL_LOGS = TagRegistry.create(new Identifier(Hallows.MOD_ID, "asphodel_logs"), BlockTags::getTagGroup);
+        public static final Tag.Identified<Block> EBONY_LOGS = TagRegistry.create(new Identifier(Hallows.MOD_ID, "ebony_logs"), BlockTags::getTagGroup);
+        public static final Tag.Identified<Block> NECROFIRE_BASE_BLOCKS = TagRegistry.create(new Identifier(Hallows.MOD_ID, "necrofire_base_blocks"), BlockTags::getTagGroup);
+        public static final Tag.Identified<Block> RED_MOSS_REPLACEABLE = TagRegistry.create(new Identifier(Hallows.MOD_ID, "red_moss_replaceable"), BlockTags::getTagGroup);
     }
 
     public static class Items {
+        public static final Tag.Identified<Item> ASPHODEL_LOGS = TagRegistry.create(new Identifier(Hallows.MOD_ID, "asphodel_logs"), ItemTags::getTagGroup);
+        public static final Tag.Identified<Item> EBONY_LOGS = TagRegistry.create(new Identifier(Hallows.MOD_ID, "ebony_logs"), ItemTags::getTagGroup);
+        public static final Tag.Identified<Item> NECROFIRE_BASE_BLOCKS = TagRegistry.create(new Identifier(Hallows.MOD_ID, "necrofire_base_blocks"), ItemTags::getTagGroup);
     }
 }
