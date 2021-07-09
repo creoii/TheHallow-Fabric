11f415c5d3a074218bf2a4ba94a74573f74730c2
creoii <33011337+creoii@users.noreply.github.com>
2021-07-09 01:35:59 -0400

    anointment table remodel

--- src/main/java/creoii/hallows/common/block/AnointmentTableBlock.java
+++ src/main/java/creoii/hallows/common/block/AnointmentTableBlock.java
@@ -4,6 +4,7 @@ import creoii.hallows.common.recipe.AnointingScreenHandler;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.CraftingTableBlock;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.screen.ScreenHandlerContext;
@@ -14,11 +15,25 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
 public class AnointmentTableBlock extends CraftingTableBlock {
+    protected static final VoxelShape SHAPE = Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 12.0D, 16.0D);
+
     public AnointmentTableBlock(Settings settings) {
         super(settings);
+    }
+
+    @SuppressWarnings("deprecation")
+    public boolean hasSidedTransparency(BlockState state) {
+        return true;
+    }
+
+    @SuppressWarnings("deprecation")
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return SHAPE;
     }
 
     @Override
